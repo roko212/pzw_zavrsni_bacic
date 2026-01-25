615dbe8eed6c599bc05fd5b3e7adf6c5b8a07bc0
Roko Baćić <roko.bacic54@gmail.com>
2026-01-25 19:59:46 +0100

    Izrađen login i registracija, model s 3 entiteta i jednom relacijom

--- moja_stranica/gymsite/main/admin.py
+++ moja_stranica/gymsite/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+model_list = [Korisnik, Trener, Trening]
+admin.site.register(model_list)
--- moja_stranica/gymsite/main/models.py
+++ moja_stranica/gymsite/main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Korisnik(models.Model):
+    korisnik_ime = models.CharField(max_length=50)
+    korisnik_datum_rodenja = models.DateField()
+    korisnik_id = models.CharField(max_length=10)
+    korisnik_mail = models.EmailField()
+
+    def __str__(self):
+        return self.korisnik_ime
+    
+class Trener(models.Model):
+    trener_ime = models.CharField(max_length=50)
+    trener_id = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.trener_ime
+    
+class Trening(models.Model):
+    trening_ime = models.CharField(max_length=20)
+    trening_vrsta = models.CharField(max_length=20)
+    trening_opis = models.TextField()
+    trening_trener = models.ForeignKey(Trener, on_delete=models.CASCADE)
